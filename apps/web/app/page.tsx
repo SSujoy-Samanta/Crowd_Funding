1ff65e6284080c38608d2e195a0b5b7c455c8b69
Sujoy Samanta <sujoysamanta5317@gmail.com>
2025-04-01 01:43:54 +0530

    update ui of hero page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,4 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import { Interaction } from "../components/Interaction";
-import { CreateCampaign} from "../components/Campaign/CreateCampaign";
-import { Account } from "../components/Account";
-import { GetDeployedContracts } from "../components/GetDepolyedContracts";
-import { CreatorCampaign } from "../components/CreatorCampaign";
-import { CampaignGoal } from "../components/CampaignGoal";
-import { FundingRaised } from "../components/FundingRaised";
-import { VotingStatus } from "../components/VotingStatus";
-import { FundingApproveStatus } from "../components/FundingApproveStatus";
-import { ContributeFund } from "../components/ContributeFund";
-import { Voting } from "../components/Voting";
-import { WithDrawFund } from "../components/WithDrawFund";
-import { ClaimRefund } from "../components/ClaimRefund";
-// import { HeroPage } from "@/components/Hero/hero";
-import VerticalVoteBarChart from "@/components/dashboard/BarChart";
 import HomePage from "@/components/Hero/HomePage";
 
 
